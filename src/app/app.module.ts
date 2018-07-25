3e46ee1681664fff0e9485e3ede1a32b442f4963
Vaibhav Shringarpure <vibs.sh@gmail.com>
2018-07-25 15:00:09 +0100

    * Adds Components and Service for the Application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { VideoListComponent } from './components/video-list/video-list.component';
+import { VideoComponent } from './components/video/video.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    VideoListComponent,
+    VideoComponent
   ],
   imports: [
     BrowserModule,

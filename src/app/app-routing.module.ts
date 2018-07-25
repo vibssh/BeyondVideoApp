64336b270273ec4ba065572ca6908d8c6abdc76f
Vaibhav Shringarpure <vibs.sh@gmail.com>
2018-07-25 20:41:45 +0100

    * Adds Routing for Video List Page * Adds styles to the card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VideoListComponent } from './components/video-list/video-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: 'video-list', component:VideoListComponent},
+    {path: '', component:VideoListComponent, pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
